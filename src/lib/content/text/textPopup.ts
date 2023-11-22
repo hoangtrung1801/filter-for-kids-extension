c1c793530cc22b84ba25a8feb442e118bcb897dd
Sang <phunganhsang123@gmail.com>
2023-11-23 00:03:22 +0700

    update

--- src/lib/content/text/textPopup.ts
+++ src/lib/content/text/textPopup.ts
@@ -1,54 +1,76 @@
-import acronymDict from "assets/dict_acronym.json"
-import wordDict from "assets/word.json"
-import iconUrl from "url:~assets/icon.jpg"
+import acronymDict from "assets/dict_acronym.json";
+import wordDict from "assets/word.json";
+import iconUrl from "url:~assets/icon.jpg";
 
+export function initTextPopup() {
+	document.addEventListener("mouseup", function (event) {
+		let mousePos = {
+			// x: event.clientX + window.scrollX,
+			// y: event.clientY + window.scrollY
+			x: event.clientX,
+			y: event.clientY
+		};
 
-export function initTextPopup(){
-    document.addEventListener("mouseup", function (event) {
-        let mousePos = {
-          x: event.clientX + window.scrollX,
-          y: event.clientY + window.scrollY,
-        };
+		console.log(getSelectedText());
+		console.log(acronymDict);
+		console.log(wordDict);
+		console.log(iconUrl);
+		console.log(mousePos);
 
-        console.log(getSelectedText());
-        console.log(acronymDict);
-        console.log(wordDict);
-        console.log(iconUrl);
-        showIcon(mousePos.x,mousePos.y);
-        
-              
-               
-    }); 
+		popupIcon(mousePos);
+	});
+}
+function popupIcon(mousePos) {
+	const iconCheck = document.querySelector("img#icon-popup");
+	const resultCheck = document.querySelector("div#result-popup");
+
+	if (
+		resultCheck &&
+		resultCheck !== event.target &&
+		!resultCheck.contains(event.target as Node)
+	) {
+		resultCheck.remove();
+	}
+
+	if (
+		iconCheck &&
+		iconCheck !== event.target &&
+		!iconCheck.contains(event.target as Node)
+	) {
+		iconCheck.remove();
+	}
+	if (getSelectedText() && getSelectedText().length > 0) {
+		console.log(getSelectedText());
+		renderIcon(mousePos);
+    renderResult(mousePos)
+	}
 }
 
-function getSelectedText() : string{
-    var selectedText : string = window.getSelection().toString();
-    return selectedText;
+function getSelectedText(): string {
+	var selectedText: string = window.getSelection().toString();
+	return selectedText;
 }
 
-
-
-function showIcon(x, y) {
-  var icon = document.createElement("img");
-  icon.src = chrome.runtime.getURL(iconUrl);
-  icon.style.position = "fixed";
-  icon.style.top = y + "px";
-  icon.style.left = x + "px";
-
-
-  document.body.appendChild(icon);
-
-  // Remove the icon after a certain time (e.g., 2 seconds)
-  setTimeout(function () {
-    document.body.removeChild(icon);
-  }, 2000);
+function renderResult(mousePos){
+  const result = document.querySelector("div#result-popup");
 }
 
+function renderIcon(mousePos) {
+	var icon = document.createElement("img");
+	icon.src = iconUrl;
+	icon.id = "icon-popup";
+	icon.style.position = "fixed";
+	icon.style.cursor = "pointer";
+	icon.style.top = mousePos.y + "px";
+	icon.style.left = mousePos.x + "px";
+
+	document.body.appendChild(icon);
+}
 
 // function loadDictionaryAcronym() {
 //     return new Promise((resolve, reject) => {
 //       const jsonFilePath = chrome.runtime.getURL("./assets/dict_acronym.json");
-  
+
 //       fetch(jsonFilePath)
 //         .then((response) => {
 //           if (!response.ok) {
@@ -60,4 +82,3 @@ function showIcon(x, y) {
 //         .catch((error) => reject(error));
 //     });
 //   }
-
