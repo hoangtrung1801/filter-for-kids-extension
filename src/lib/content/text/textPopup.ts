66a7cfd76eaf9ae1a421a18e6b7227e5799c9f5b
Sang <phunganhsang123@gmail.com>
2023-11-27 22:32:35 +0700

    sang

--- src/lib/content/text/textPopup.ts
+++ src/lib/content/text/textPopup.ts
@@ -1,13 +1,17 @@
 import acronymDict from "assets/dict_acronym.json";
 import wordDict from "assets/word.json";
 import iconUrl from "url:~assets/icon.jpg";
+import containerStyle from "data-text:~css/container.css"
+
+function searchAcronym(jsonData, selectedText) {
+	return jsonData[selectedText];
+}
 
 export function initTextPopup() {
 	document.addEventListener("mouseup", popupIcon);
 }
 
 function popupIcon(event) {
-	console.log(chrome.runtime.getURL("/assets/icon.svg"));
 	let mousePos = {
 		x: event.clientX + window.scrollX,
 		y: event.clientY + window.scrollY
@@ -31,23 +35,24 @@ function popupIcon(event) {
 		iconCheck.remove();
 	}
 	if (getSelectedText() && getSelectedText().length > 0) {
-		// console.log(getSelectedText());
 		renderIcon(mousePos);
 		const icon = document.querySelector("div#icon-container");
 		if (icon) {
-			// document.removeEventListener("mouseup", popupIcon);
+			var selectedText = getSelectedText();
 			icon.addEventListener("click", async () => {
+				console.log(searchAcronym(acronymDict, selectedText));
+				console.log(acronymDict);
+				console.log(selectedText);
+
 				const iconn = document.querySelector("div#icon-container");
 				iconn.remove();
+
 				renderResult(
 					mousePos,
-					getSelectedText(),
-					getJsonDetailWord(),
-					searchAcronym(acronymDict, getSelectedText())
+					selectedText,
+					wordDict,
+					searchAcronym(acronymDict, selectedText)
 				);
-				console.log(searchAcronym(acronymDict,getSelectedText()));
-				
-				// document.addEventListener("mouseup", popupIcon);
 			});
 		}
 	}
@@ -66,50 +71,124 @@ function renderResult(mousePos, selectedText, detail, acronym) {
 	console.log(mousePos);
 	const resultContainer = document.createElement("div");
 	resultContainer.id = "result-popup";
-
 	fetch(chrome.runtime.getURL("/html/container.html"))
 		.then((response) => response.text())
 		.then((htmlContent) => {
 			resultContainer.innerHTML = htmlContent;
-			// const wordSpan = resultContainer.querySelector("#word-span");
-			// const definitionSpan =
-			// 	resultContainer.querySelector("#definition-span");
-			// const exampleSpan1 =
-			// 	resultContainer.querySelector("#example-span1");
-			// const exampleSpan2 =
-			// 	resultContainer.querySelector("#example-span2");
-			// const acronymSpan = resultContainer.querySelector("#acronym-span");
-			// const synonymSpan = resultContainer.querySelector("#synonym-span");
-			// const antonymSpan = resultContainer.querySelector("#antonym-span");
+			console.log(detail);
+			const acronymDiv = resultContainer.querySelector("#acronym");
+			const nounDiv = resultContainer.querySelector("#noun");
+			const verbDiv = resultContainer.querySelector("#verb");
+			const adjDiv = resultContainer.querySelector("#adj");
+			const wordSpan = resultContainer.querySelector("#word-span");
+			const acronymSpan = resultContainer.querySelector("#acronym-span");
+			const definationSpanNoun = resultContainer.querySelector(
+				"#noun #definition-span"
+			);
+			const exampleSpan1Noun = resultContainer.querySelector(
+				"#noun #example-span1"
+			);
+			const exampleSpan2Noun = resultContainer.querySelector(
+				"#noun #example-span2"
+			);
+			const synonymSpanNoun = resultContainer.querySelector(
+				"#noun #synonym-span"
+			);
+			const antonymSpanNoun = resultContainer.querySelector(
+				"#noun #antonym-span"
+			);
+			// if (
+			// 	typeof acronym === "undefined"
+			// ) {
+			// 	acronymDiv.style.display = "none";
+			// }
+			if (
+				typeof detail[selectedText] === "undefined" ||
+				(typeof detail[selectedText].noun[0].defination === "string" &&
+					detail[selectedText].noun[0].defination.length === 0)
+			) {
+				nounDiv.style.display = "none";
+			}
+			if (
+				typeof detail[selectedText] === "undefined" ||
+				(typeof detail[selectedText].verb[0].defination === "string" &&
+					detail[selectedText].verb[0].defination.length === 0)
+			) {
+				verbDiv.style.display = "none";
+			}
+			if (
+				typeof detail[selectedText] === "undefined" ||
+				(typeof detail[selectedText].adj[0].defination === "string" &&
+					detail[selectedText].adj[0].defination.length === 0)
+			) {
+				adjDiv.style.display = "none";
+			}
 
-			// if (wordSpan) {
-			// 	wordSpan.textContent = selectedText;
-			// }
-			// if (definitionSpan) {
-			// 	definitionSpan.textContent =
-			// 		detail[selectedText].noun[0].definition;
-			// }
-			// if (exampleSpan1) {
-			// 	exampleSpan1.textContent =
-			// 		detail[selectedText].noun[0].example[0];
-			// }
-			// if (exampleSpan2) {
-			// 	exampleSpan2.textContent =
-			// 		detail[selectedText].noun[0].example[1];
-			// }
-			// if (acronymSpan) {
-			// 	acronymSpan.textContent = acronym;
-			// }
-			// if (synonymSpan) {
-			// 	synonymSpan.textContent =
-			// 		detail[selectedText].noun[0].synonyms[0];
-			// }
-			// if (antonymSpan) {
-			// 	antonymSpan.textContent =
-			// 		detail[selectedText].noun[0].antonyms[0];
-			// }
+			if (acronymSpan) {
+				acronymSpan.textContent = acronym;
+				console.log(acronym);
+			}
+			if (wordSpan) {
+				wordSpan.textContent = selectedText;
+			}
+			if (definationSpanNoun) {
+				definationSpanNoun.textContent =
+					detail[selectedText].noun[0].defination;
+			}
+			if (exampleSpan1Noun) {
+				exampleSpan1Noun.textContent =
+					detail[selectedText].noun[0].example[0];
+			}
+			if (exampleSpan2Noun) {
+				exampleSpan2Noun.textContent =
+					detail[selectedText].noun[0].example[1];
+			}
+			if (synonymSpanNoun) {
+				synonymSpanNoun.textContent =
+					detail[selectedText].noun[0].synonyms[0];
+			}
+			if (antonymSpanNoun) {
+				antonymSpanNoun.textContent =
+					detail[selectedText].noun[0].antonyms[0];
+			}
+
+			const definitionSpanVerb = resultContainer.querySelector(
+				"#verb #definition-span"
+			);
+			const exampleSpan1Verb = resultContainer.querySelector(
+				"#verb #example-span1"
+			);
+			const exampleSpan2Verb = resultContainer.querySelector(
+				"#verb #example-span2"
+			);
+			const synonymSpanVerb = resultContainer.querySelector(
+				"#verb #synonym-span"
+			);
+			const antonymSpanVerb = resultContainer.querySelector(
+				"#verb #antonym-span"
+			);
+			if (definitionSpanVerb) {
+				definitionSpanVerb.textContent =
+					detail[selectedText].verb[0].defination;
+			}
+			if (exampleSpan1Verb) {
+				exampleSpan1Verb.textContent =
+					detail[selectedText].verb[0].example[0];
+			}
+			if (exampleSpan2Verb) {
+				exampleSpan2Verb.textContent =
+					detail[selectedText].verb[0].example[1];
+			}
+			if (synonymSpanVerb) {
+				synonymSpanVerb.textContent =
+					detail[selectedText].verb[0].synonyms[0];
+			}
+			if (antonymSpanVerb) {
+				antonymSpanVerb.textContent =
+					detail[selectedText].verb[0].antonyms[0];
+			}
 		});
-
+	
 	resultContainer.style.background = "white";
 	resultContainer.style.color = "black";
 	resultContainer.style.fontSize = "15px";
@@ -135,32 +214,6 @@ function renderIcon(mousePos) {
 	iconContainer.style.top = mousePos.y + "px";
 	iconContainer.style.left = mousePos.x + "px";
 	console.log(mousePos);
-	
+
 	document.body.appendChild(iconContainer);
 }
-
-function searchAcronym(jsonData, selectedText) {
-	return jsonData[selectedText];
-}
-function getJsonDetailWord() {
-	const jsonFilePath = chrome.runtime.getURL("/dictionary/test.json");
-	return fetch(jsonFilePath)
-		.then((response) => response.json())
-		.catch((error) => console.error("Error loading JSON file:", error));
-}
-
-// function loadDictionaryAcronym() {
-//     return new Promise((resolve, reject) => {
-//       const jsonFilePath = chrome.runtime.getURL("./assets/dict_acronym.json");
-
-//       fetch(jsonFilePath)
-//         .then((response) => {
-//           if (!response.ok) {
-//             throw new Error(`HTTP error! Status: ${response.status}`);
-//           }
-//           return response.json();
-//         })
-//         .then((json) => resolve(json))
-//         .catch((error) => reject(error));
-//     });
-//   }
