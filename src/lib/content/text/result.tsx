afb4d904f816549b822a8665acab45af1f3f1397
Sang <phunganhsang123@gmail.com>
2024-01-14 10:34:12 +0700

    sang

--- src/lib/content/text/result.tsx
+++ src/lib/content/text/result.tsx
@@ -2,17 +2,20 @@ import acronymDict from "assets/dict_acronym.json";
 import wordDict from "assets/word.json";
 
 
-export default function ResultPopup(props) {
-    function searchAcronym(jsonData, selectedText) {
-        return jsonData[selectedText];
-    }
+function searchAcronym(jsonData, selectedText) {
+    return jsonData[selectedText];
+}
 
+export default function ResultPopup(props) {
+    console.log(wordDict[props.selectedText].img[0].url);
+    
     return (
         <div id="result" style={{ position: "absolute", background: "white", top: props.mousePos.y, left: props.mousePos.x }} className="container">
             <div>Word:</div>
             <div>{props.selectedText}</div>
-            <div>Acronym:</div>
-            <div>{searchAcronym(acronymDict,props.selectedText)}</div>
+            {/* <div>Acronym:</div>
+            <div>{searchAcronym(acronymDict,props.selectedText)}</div> */}
+            <img src={wordDict[props.selectedText].img[0].url} alt="Image Not Available" />
         </div>
     );
 }
