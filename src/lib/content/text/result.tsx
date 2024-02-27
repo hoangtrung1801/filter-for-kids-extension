56de81e96f767913919a98498f8a72ad578ec660
Sang <phunganhsang123@gmail.com>
2024-02-28 02:43:36 +0700

    28/2/2024 update logic

--- src/lib/content/text/result.tsx
+++ src/lib/content/text/result.tsx
@@ -13,62 +13,77 @@ function searchAcronym(jsonData, selectedText) {
 
 export default function ResultPopup(props) {
 
-    const [selectedButton, setSelectedButton] = useState(null);
-
-    let contentText = [];
-
+    // let contentText = [];
     let acronym = searchAcronym(acronymDict,props.selectedText)
     let showAcronym;
     let showSinoViet;
     let showWord;
     let hanvietWord;
-    if (!(typeof acronym === "undefined")) {
-        showAcronym = true;
-        showWord = false;
-        showSinoViet= false;
-    }
-    else if(hanviet.words.find(word => word.hanviet === props.selectedText)){
-        showAcronym = false;
-        showWord = false;
-        showSinoViet= true;
-        hanvietWord = hanviet.words.find(word => word.hanviet === props.selectedText).meaning.split("/").join(", ")	
-    }
-    else{
-        showAcronym = false;
-        showWord = true;
-        showSinoViet= false;
-    }
-
-    if (
-        !(typeof wordDict[props.selectedText] === "undefined" ||
-        (typeof wordDict[props.selectedText].noun[0].defination === "string" &&
-            wordDict[props.selectedText].noun[0].defination.length === 0))
-    ) {
-        console.log("have noun");
-        contentText = [wordDict[props.selectedText].noun[0].defination,wordDict[props.selectedText].noun[0].example,wordDict[props.selectedText].noun[0].synonyms,wordDict[props.selectedText].noun[0].antonyms,wordDict[props.selectedText].img[0].url];
-    }
-    else if (
-        !(typeof wordDict[props.selectedText] === "undefined" ||
-        (typeof wordDict[props.selectedText].verb[0].defination === "string" &&
-            wordDict[props.selectedText].verb[0].defination.length === 0))
-    ) {
-        console.log("have verb");
-        contentText = [wordDict[props.selectedText].verb[0].defination,wordDict[props.selectedText].verb[0].example,wordDict[props.selectedText].verb[0].synonyms,wordDict[props.selectedText].verb[0].antonyms,wordDict[props.selectedText].img[0].url];
-    }
-    else if (
-        !(typeof wordDict[props.selectedText] === "undefined" ||
-        (typeof wordDict[props.selectedText].adj[0].defination === "string" &&
-            wordDict[props.selectedText].adj[0].defination.length === 0))
-    ) {
-        console.log("have adj");
-        contentText = [wordDict[props.selectedText].adj[0].defination,wordDict[props.selectedText].adj[0].example,wordDict[props.selectedText].adj[0].synonyms,wordDict[props.selectedText].adj[0].antonyms,wordDict[props.selectedText].img[0].url];
-        
-    }
+    let haveNoun;
+    let haveVerb;
+    let haveAdj;
     
-    const [typeWord, setTypeWord] = useState(contentText)
+    const [selectedButton, setSelectedButton] = useState(null);
+    const [contentWord, setContentWord] = useState([])
+
+    // check han viet, viet tat
+        if (!(typeof acronym === "undefined")) {
+            showAcronym = true;
+            showWord = false;
+            showSinoViet= false;
+        }
+        else if(hanviet.words.find(word => word.hanviet === props.selectedText)){
+            showAcronym = false;
+            showWord = false;
+            showSinoViet= true;
+            hanvietWord = hanviet.words.find(word => word.hanviet === props.selectedText).meaning.split("/").join(", ")	
+        }
+        else{
+            showAcronym = false;
+            showWord = true;
+            showSinoViet= false;
+        }
+
+    useEffect(() =>{
+        if(contentWord.length==0){
+            // data ban dau
+            if (
+                !(typeof wordDict[props.selectedText] === "undefined" ||
+                (typeof wordDict[props.selectedText].noun[0].defination === "string" &&
+                    wordDict[props.selectedText].noun[0].defination.length === 0))
+            ) {
+                console.log("have noun");
+                let contentText = [wordDict[props.selectedText].noun[0].defination,wordDict[props.selectedText].noun[0].example,wordDict[props.selectedText].noun[0].synonyms,wordDict[props.selectedText].noun[0].antonyms,wordDict[props.selectedText].img[0].url];
+                setContentWord(contentText);
+                setSelectedButton("noun");
+            }
+            else if (
+                !(typeof wordDict[props.selectedText] === "undefined" ||
+                (typeof wordDict[props.selectedText].verb[0].defination === "string" &&
+                    wordDict[props.selectedText].verb[0].defination.length === 0))
+            ) {
+                console.log("have verb");
+                let contentText = [wordDict[props.selectedText].verb[0].defination,wordDict[props.selectedText].verb[0].example,wordDict[props.selectedText].verb[0].synonyms,wordDict[props.selectedText].verb[0].antonyms,wordDict[props.selectedText].img[0].url];
+                setContentWord(contentText);
+                setSelectedButton("verb");
+            }
+            else if (
+                !(typeof wordDict[props.selectedText] === "undefined" ||
+                (typeof wordDict[props.selectedText].adj[0].defination === "string" &&
+                    wordDict[props.selectedText].adj[0].defination.length === 0))
+            ) {
+                console.log("have adj");
+                let contentText = [wordDict[props.selectedText].adj[0].defination,wordDict[props.selectedText].adj[0].example,wordDict[props.selectedText].adj[0].synonyms,wordDict[props.selectedText].adj[0].antonyms,wordDict[props.selectedText].img[0].url];
+                setContentWord(contentText);
+                setSelectedButton("adj");
+            }
+        }
+        
+    },[selectedButton])
+    
 
     const handleTypeWord = (in4,type) => {
-        setTypeWord(in4);
+        setContentWord(in4);
         setSelectedButton(type);
     }
    
@@ -97,13 +112,13 @@ export default function ResultPopup(props) {
             {showWord &&(
             <div className="content-itv">
                 <div className="content-text-itv">
-                    <div className="word-explain"><span>Mô tả</span>{typeWord[0]}</div>
-                    <div className="word-example"><span>Ví dụ:</span>{typeWord[1].join(", ")}</div>
-                    <div className="syntonym"><span>Đồng nghĩa:</span>{typeWord[2].join(", ")}</div>
-                    <div className="antonym"><span>Trái nghĩa</span>{typeWord[3].join(", ")}</div>
+                    <div className="word-explain"><span>Mô tả</span>{contentWord[0]}</div>
+                    <div className="word-example"><span>Ví dụ:</span>{contentWord[1]}</div>
+                    <div className="syntonym"><span>Đồng nghĩa:</span>{contentWord[2]}</div>
+                    <div className="antonym"><span>Trái nghĩa</span>{contentWord[3]}</div>
                 </div>
                 <div className="content-img-itv">
-                    <img style={{ width:"250px" }} src={typeWord[4]} alt="" />
+                    <img style={{ width:"250px" }} src={contentWord[4]} alt="" />
                 </div>
             </div>
             )}
